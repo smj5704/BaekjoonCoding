1d103b2002e4ce735cac79dc8dff035b8df2909e
SAMSUNG PC <smj5704@naver.com>
2022-04-20 18:33:09 +0900

    백준 문제풀이 20220420

---
+++ src/Baekjoon1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        sc.close();
+
+        int result = A + B ;
+        System.out.println(result);
+    }
+}
---
+++ src/Baekjoon10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baekjoon10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.nextLine();
+        sc.close();
+
+        System.out.println(id + "??!");
+    }
+}
