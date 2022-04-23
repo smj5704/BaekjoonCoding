60bd42847963e8337f7f4fd9e92f323e149417e2
SAMSUNG PC <smj5704@naver.com>
2022-04-24 03:08:13 +0900

    백준 문제풀이 20220424 15552번 문제 - scanner로 푸는 방법 t번 만큼 a,b의 합을 구하기.

--- src/Baekjoon15552_1.java
+++ src/Baekjoon15552_1.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class Baekjoon10950 {
+public class Baekjoon15552_1 {
+//    스캐너 이용하는 방법
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         int arr[] = new int[T];
 
-//        T번 루프 돌아감
-        for(int i = 0; i <= T; i++) {
+        for(int i = 0; i<= T; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
-//            더한값을 배열에 넣음
-            arr[i] = A+B;
+
+            arr[i] = A + B;
         }
+
         sc.close();
 
         for(int k : arr) {
