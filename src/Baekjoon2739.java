f3b86fff353bf4a7792a9a0e5dd95f223bd9ba4c
SAMSUNG PC <smj5704@naver.com>
2022-04-24 02:23:17 +0900

    백준 문제풀이 20220424

---
+++ src/Baekjoon2739.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Baekjoon2739 {
+    public static void main(String[] args) {
+        //구구단
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int result ;
+        if(n >= 1 && n <= 9){
+            for(int i = 1; i < 10; i ++){
+                result = n * i;
+                System.out.print(n + " * " + i + " = " );
+                System.out.println(result);
+            }
+        }
+
+    }
+}
