15486b000c71af3b911c5365d24bde43647329f1
SAMSUNG PC <smj5704@naver.com>
2022-04-27 23:00:51 +0900

    백준 문제풀이 20220427 10951번 문제 두 수를 입력 받아서 합을 출력시킴. 더이상 입력받을 값이 없을 때 출력을 끝낸다.(EOF설정) -> 방법 2) BufferReader사용

---
+++ src/Baekjoon10951_2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon10951_2 {
+    //    BufferedReader방법
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        출력은 StringBuilder로 출력
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+//            nextToken()은 문자열을 반환하기 때문에 int형으로 변환시켜줘야한다
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if(0<A && 0<B && B<10){
+               sb.append(A+B).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
