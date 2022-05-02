4f7e5b138f91fc4a9589f45477bbcc06891278de
SAMSUNG PC <smj5704@naver.com>
2022-05-02 19:45:16 +0900

    백준 문제풀이 20220429 2577번 문제 방법 1 ) scanner 세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하기

---
+++ src/Baekjoon2577_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Baekjoon2577_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int result = A * B * C;
+        String resultStr = Integer.toString(result);
+
+
+
+        for(int i = 0; i < 10; i++) {
+            int count = 0;
+            for(int j = 0; j < resultStr.length(); j++) {
+//                반드시 charAt() 을 쓸 경우  - '0' 또는 -48 을 연산해야 한다. (아니면 아스키코드에 대응하는 문자가 나온다.)
+                if((resultStr.charAt(j)-'0') == i) {
+                    count++;
+                }
+            }
+            System.out.println(i + " : " +count);
+        }
+    }
+}
