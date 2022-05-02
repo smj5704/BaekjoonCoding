e05096767a8b03d4a502241c07cc5584c92b0075
SAMSUNG PC <smj5704@naver.com>
2022-05-02 20:07:18 +0900

    백준 문제풀이 20220502 15552_2, 18108번 문제재 업로드

---
+++ src/Baekjoon18108.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Baekjoon18108 {
+        //불기년도 -> 서기년도 문제
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+//        불기년도 = 서기년도 + 544
+            int BudYear = sc.nextInt();
+            int ADYear = BudYear - 543;
+            sc.close();
+            System.out.println(ADYear);
+
+        }
+    }
+
+
