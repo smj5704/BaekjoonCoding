fd6bfef1dde77ef2b3321d93672f41ffee4cd812
SAMSUNG PC <smj5704@naver.com>
2022-05-02 20:26:46 +0900

    백준 문제풀이 20220502 2563번 문제 방법 3 ) BuffredReader2_배열사용x n개의 배열에서 최댓값과 그 최댓값이 배열에서 몇 번 째 원소인지 출력

---
+++ src/Baekjoon2562_3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2562_3 {
+    public static void main(String[] args) throws IOException {
+        //배열을 쓰지 않는 방법
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < n; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if(value > max) {
+                max = value;
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+
+
+    }
+}
