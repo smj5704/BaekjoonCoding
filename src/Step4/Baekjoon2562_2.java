ba2b36de8f193c43350c794a5bf83442c9b3c663
SAMSUNG PC <smj5704@naver.com>
2022-05-03 14:27:19 +0900

    패키지 이동

--- src/Step4/Baekjoon2562_2.java
+++ src/Step4/Baekjoon2562_2.java
@@ -1,25 +1,34 @@
+package Step4;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Baekjoon2562_3 {
+public class Baekjoon2562_2 {
     public static void main(String[] args) throws IOException {
-        //배열을 쓰지 않는 방법
+//        BufferedReader방법
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+
+        int count = 0;
         int max = 0;
         int index = 0;
-        for(int i = 0; i < n; i++) {
-            int value = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        for(int value : arr) {
+            count++;
             if(value > max) {
                 max = value;
-                index = i+1;
+                index = count;
             }
         }
         System.out.println(max);
         System.out.println(index);
-
-
     }
 }
--- src/Step4/Baekjoon2562_3.java
+++ src/Step4/Baekjoon2562_3.java
@@ -1,32 +1,27 @@
+package Step4;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Baekjoon2562_2 {
+public class Baekjoon2562_3 {
     public static void main(String[] args) throws IOException {
-//        BufferedReader방법
+        //배열을 쓰지 않는 방법
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n];
 
-        int count = 0;
+        int n = Integer.parseInt(br.readLine());
         int max = 0;
         int index = 0;
-
-        for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        br.close();
-
-        for(int value : arr) {
-            count++;
+        for(int i = 0; i < n; i++) {
+            int value = Integer.parseInt(br.readLine());
             if(value > max) {
                 max = value;
-                index = count;
+                index = i+1;
             }
         }
         System.out.println(max);
         System.out.println(index);
+
+
     }
 }
