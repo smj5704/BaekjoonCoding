2a8e6ff633fbe30a75b8ec6b07c027b6dbb3950f
SAMSUNG PC <smj5704@naver.com>
2022-05-08 23:35:08 +0900

    백준 문제풀이 20220508 11654번 문제 아스키 코드 출력하기_2 : System.in 사용

---
+++ src/Step6/Baekjoon11654_2.java
@@ -0,0 +1,11 @@
+package Step6;
+
+public class Baekjoon11654_2 {
+    public static void main(String[] args) throws Exception{
+        //System.in 은 byte 값으로 문자 한 개만 읽으면서 해당 문자에 대응되는 아스키코드 값을 저장할 수 있다.
+        //단, 예외처리 중요
+        int str = System.in.read();
+        System.out.println(str);
+
+    }
+}
